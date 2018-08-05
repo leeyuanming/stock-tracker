046b7db1b98e8248b0c0d8fb01042a09ddc43582
yuanming.lee91 <yuanming.lee91@gmail.com>
2018-08-05 14:48:26 -0400

    Lecture 182, added user first and last names to registration page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,17 +1,11 @@
 class StocksController < ApplicationController
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        render partial: 'users/result'
-      else
-        flash[:danger] = "You have entered an incorrect symbol"
-        redirect_to my_portfolio_path
-      end
-    else
+    if params[:stock].blank?
       flash[:danger] = "You have entered an empty search string"
-      redirect_to my_portfolio_path
+    else
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash[:danger] = "You have entered an incorrect symbol" unless @stock
     end
+    render partial: 'users/result'
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => 'user/registrations'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :user_stocks, only:[:create, :destroy]
 
   # Example resource route with options:
   #   resources :products do
